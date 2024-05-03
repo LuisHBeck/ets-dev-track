8f384ebe2f7888b8a5ed75f2040c451dcec45d38
LuisHBeck <beck.henrique.luis@gmail.com>
2024-05-03 14:18:32 -0300

    feat: finish login form appearence

--- fullstack-login-page/angular-login-page/src/app/components/primary-input/primary-input.component.ts
+++ fullstack-login-page/angular-login-page/src/app/components/primary-input/primary-input.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, Input, forwardRef, input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 
 type InputTypes = "text" | "email" | "password"
 
@@ -9,11 +9,42 @@ type InputTypes = "text" | "email" | "password"
   imports: [
     ReactiveFormsModule
   ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => PrimaryInputComponent),
+      multi: true
+    }
+  ],
   templateUrl: './primary-input.component.html',
   styleUrl: './primary-input.component.scss'
 })
-export class PrimaryInputComponent {
+export class PrimaryInputComponent implements ControlValueAccessor {
   @Input() type: InputTypes = "text";
   @Input() placeholder: string = "";
-  @Input() formName: string = "";
+  @Input() label: string = "";
+  @Input() inputName: string = "";
+
+  value: string = '';
+  onChange: any = () => {};
+  onTouch: any = () => {};
+
+  onInput(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.onChange(value);
+  }
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {}
 }
