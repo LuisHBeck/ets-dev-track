0c31206ac906e276bce3fca630770c5ee255b3f3
LuisHBeck <beck.henrique.luis@gmail.com>
2024-05-03 11:08:39 -0300

    feat: start login page and input component

---
+++ fullstack-login-page/angular-login-page/src/app/components/primary-input/primary-input.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+type InputTypes = "text" | "email" | "password"
+
+@Component({
+  selector: 'app-primary-input',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule
+  ],
+  templateUrl: './primary-input.component.html',
+  styleUrl: './primary-input.component.scss'
+})
+export class PrimaryInputComponent {
+  @Input() type: InputTypes = "text";
+  @Input() placeholder: string = "";
+  @Input() formName: string = "";
+}
