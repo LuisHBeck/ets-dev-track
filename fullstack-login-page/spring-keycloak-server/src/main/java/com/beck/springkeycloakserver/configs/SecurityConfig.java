f1f3c53a6077f509820c7ae084009aaaaa62d4e6
LuisHBeck <beck.henrique.luis@gmail.com>
2024-05-09 08:59:43 -0300

    feat: cors config

--- fullstack-login-page/spring-keycloak-server/src/main/java/com/beck/springkeycloakserver/configs/SecurityConfig.java
+++ fullstack-login-page/spring-keycloak-server/src/main/java/com/beck/springkeycloakserver/configs/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -15,6 +16,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req -> req
                         .anyRequest().permitAll())
                 .oauth2Client(withDefaults())
